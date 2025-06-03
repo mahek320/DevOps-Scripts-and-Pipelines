ce0a8e0cae3e79ef076cafed32b1ab80e98515d7
mahek320 <Mahek.10748211@LTIMindtree.com>
2025-06-03 19:46:34 +0530

    Create book_library.py

---
+++ src/book_library.py
@@ -0,0 +1,13 @@
+class BookLibrary:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, book):
+        if book in self.books:
+            self.books.remove(book)
+
+    def get_books(self):
+        return self.booksimport os  
